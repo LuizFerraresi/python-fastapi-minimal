ae4bad20835ed51f71ec5d9b3054768aca80160e
Luiz Felipe Ferraresi <luiz.f_ferraresi@hotmail.com>
2026-03-16 15:02:00 -0300

    added fastapi api entrypoint

---
+++ app/api.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+from .settings import get_settings, ApplicationSettings
+from .routes import router
+
+def application(settings: ApplicationSettings) -> FastAPI:
+    _app = FastAPI(
+        title=settings.name,
+        version=settings.version,
+        debug=settings.debug_enabled
+    )
+    _app.include_router(router)
+    return _app
+
+app = application(get_settings())
