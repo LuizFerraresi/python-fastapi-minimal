23022a8189ea665b123d157805be628344f6554a
Luiz Felipe Ferraresi <luiz.f_ferraresi@hotmail.com>
2026-03-20 14:26:19 -0300

    enhanced entrypoints and removed logic

--- app/entrypoints/routes/__init__.py
+++ app/entrypoints/routes/__init__.py
@@ -1,5 +1,8 @@
 from fastapi import APIRouter
 
+from app.entrypoints.routes.system import router as system
+
 router = APIRouter(prefix="/api")
+router.include_router(system)
 
 __all__ = ["router"]
